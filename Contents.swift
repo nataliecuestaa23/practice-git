a5ccbe37b56278cde4d74a8ce1caa7658361a185
Shalini Kishore <shaliniskishore@gmail.com>
2019-07-08 15:45:34 -0500

    Initial Commit

---
+++ Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+//this is a comment! Please use comments to keep track of your code!
+//Intention : I would like yo create a piece of code that would take each scholar's and output each name into a string.
+//Data types involved: Strings
+//var name = "Natalie "
+//var name2 = "Nailah"
+//print ("Hi \(name)"+"and "+"\( name2)! We are so happy you're here at kWk Portland!")
+
+//Intention: Working with integers doubles
+// Data types involved: Ints and doubles
+
+var a = 12.4
+var b = 65.0
+var c = 31.0
+var d = 98
+
+print (((a + b + c)+Double( d))/4)
+
+
+
